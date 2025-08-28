15e900c9a631891a92e100ba4b592e342f956f55
Adeola Olanrewaju <olanrewajuadeola58@gmail.com>
2025-08-28 07:43:04 +0100

    Part 4: Added serializers, DRF views, URLs, fixtures, and homepage

--- budget_tracker/settings.py
+++ budget_tracker/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "tracker",  # our app
+    "rest_framework",  # add this
+    "tracker",         # my app
+
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",  # <-- this gives you forms
+    ]
+}
+
--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import Category, Transaction
+from .serializers import CategorySerializer, TransactionSerializer
+
+# This will control Category endpoints
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+# This will control Transaction endpoints
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({
+        "message": "Welcome to Budget Tracker API 👋",
+        "endpoints": {
+            "Categories": "/api/categories/",
+            "Transactions": "/api/transactions/"
+        }
+    })
